1de64fe2d9bf0fd9e097a23a83ec149b33366f39
Arnabjyoti Kalita <arnabjyotikalita35@gmail.com>
2016-12-21 03:05:03 -0500

    Added codegen for functionentry and others

--- STEClasses.C
+++ STEClasses.C
@@ -145,6 +145,12 @@ void EventEntry::typePrint(ostream& out, int indent) const
 	out << endl;
 }
 
+void EventEntry::codeGen() const
+{
+	// TODO:: Event codegen
+
+}
+
 
 void ClassEntry::print(ostream& out, int indent) const
 {
@@ -160,9 +166,12 @@ void ClassEntry::typePrint(ostream& out, int indent) const
 	out << endl;
 }
 
+void ClassEntry::codeGen() const
+{
+	// nothing much to do
+	return;
 
-
-
+}
 
 void VariableEntry::print(ostream& out, int indent) const
 {	
@@ -192,6 +201,127 @@ void VariableEntry::typePrint(ostream& out, int indent) const
 	 
 }
 
+void VariableEntry::codeGen() const
+{
+	// again differentiate based on whether the variable is local/global/param based
+
+	Instruction* addGlobalVar;
+	Instruction* storeVarValue;
+
+	Instruction* addLocalVar;
+	Instruction* addParamVar;
+
+	Instruction* getParamIns;
+
+	bool isInt;
+
+	// determine type of global var
+	if(Type::isIntegral(type()->tag()) || Type::isBool(type()->tag())){
+		isInt = true;
+		setReg(intReg());  // sets integer register for this variable
+	}
+
+	if(Type::isFloat(type()->tag())){
+		isInt = false;
+		setReg(floatReg()); // sets floating point register for this variable
+	}
+
+	if(varKind() == VariableEntry::VarKind::GLOBAL_VAR){
+		addGlobalVar = new ArithIns(ArithIns::ArithInsType::ADD);
+		addGlobalVar->operand1(get_global_reg(), NULL); // get me global register
+
+		const Value* offset = new Value(offSet(), Type::TypeTag::INT);
+		addGlobalVar->operand2(-1, offSet);  // add offset value to this register
+
+		
+		intReg = intReg();  // give me an integer register
+		addGlobalVar->operandDest(intReg, NULL);
+		
+		addGlobalVar->print();
+		
+		// if you have initial value--> store it in this register
+		if(initVal() != NULL){
+			if(isInt){
+				initVal()->codeGen();  // call codeGen , have a register with it
+				storeVarValue = new MovIns(MovIns::MovInsType::STI);
+				
+				storeVarValue->operand1(initVal()->getReg(), NULL); // get register associated with value
+			}
+			else{
+				initVal()->codeGen();  // call codeGen , have a register with it
+				storeVarValue = new MovIns(MovIns::MovInsType::STF);
+				
+				storeVarValue->operand1(initVal()->getReg(), NULL); // get register associated with value
+			}
+			storeVarValue->operandDest(intReg, NULL);
+
+			storeVarValue->print();
+		} 
+	}
+	else if(varKind() == VariableEntry::VarKind::LOCAL_VAR){
+		addLocalVar = new ArithIns(ArithIns::ArithInsType::SUB); // subtract local variables from base pointer reg.
+
+		addLocalVar->operand1(get_base_ptr_reg(), NULL); // get me base pointer register
+
+		const Value* offset = new Value(offSet(), Type::TypeTag::INT);
+
+		addLocalVar->operand2(-1, offSet);  // add offset value to this register
+
+		
+		intReg = intReg();  // give me an integer register
+		addLocalVar->operandDest(intReg, NULL);
+		
+		addLocalVar->print();
+		
+		// if you have initial value--> store it in this register
+		if(initVal() != NULL){
+			if(isInt){
+				initVal()->codeGen();  // call codeGen , have a register with it
+				storeVarValue = new MovIns(MovIns::MovInsType::STI);
+				
+				storeVarValue->operand1(initVal()->getReg(), NULL); // get register associated with value
+			}
+			else{
+				initVal()->codeGen();  // call codeGen , have a register with it
+				storeVarValue = new MovIns(MovIns::MovInsType::STF);
+				
+				storeVarValue->operand1(initVal()->getReg(), NULL); // get register associated with value
+			}
+			storeVarValue->operandDest(intReg, NULL);
+
+			storeVarValue->print();
+		}
+	}
+	else{
+			// PARAM VARS
+			addParamVar = new ArithIns(ArithIns::ArithInsType::ADD); // add local variables from base pointer register.
+
+			addParamVar->operand1(get_tmp_stck_ptr_reg(), NULL); // get me temporary stack pointer register-- as you saw in code
+																	// for stack pointer 900-> I want 901
+
+			const Value* offset = new Value(1, Type::TypeTag::INT);
+			addParamVar->operand2(-1, offSet);  // add offset value to this register
+			addParamVar->operandDest(get_tmp_stck_ptr_reg(), NULL); // ADD 901 1 901
+		
+			addParamVar->print();
+		
+			// 
+			if(isInt){
+				getParamIns = new MovIns(MovIns::MovInsType::LDI);  // load from memory to registers
+
+			}
+			else{
+				getParamIns = new MovIns(MovIns::MovInsType::LDF);
+			}
+			getParamIns->operand1(get_tmp_stck_ptr_reg(), NULL);
+			getParamIns->operandDest(getReg(), NULL); // LDI R901 R$$$
+
+			getParamIns->print();
+		} 
+	}
+
+}
+
 /* simple memory allocation functions */
 
 void VariableEntry::memAlloc()
@@ -209,14 +339,6 @@ void VariableEntry::memAlloc()
 		
 		this->offSet(countLocal);
 	}
-}
-
-void VariableEntry::codeGen(){
-
-
-
-
-
 }
 
 
@@ -296,6 +418,137 @@ void FunctionEntry::typePrint(ostream& out, int indent) const
 
 void FunctionEntry::codeGen() const
 {
+	int num_local_var = 0;
+	int num_actual_param = 0;
+	// many things happen when function entry happens
+	// 1. label gets generated.
+	Instruction* labelIns;
+	labelIns = new LabelIns();
+	labelIns->funLabel(name()); // function label 
+	labelIns->print();
+
+	//2. make base pointer equal to stack pointer
+	Instruction* bpsp;
+	bpsp = new MovIns(MovIns::MovInsType::MOVI);
+
+	bpsp->operand1(get_stck_ptr_reg(), NULL); // stack 
+	bpsp->operandDest(get_base_ptr_reg(), NULL);
+
+	bpsp->print();
+
+	//3. generate a temporary_stack_ptr register and add 1
+	Instruction* tpsp;
+	tpsp = new ArithIns(ArithIns::ArithInsType::ADD);
+	tpsp->operand1(get_stck_ptr_reg(), NULL);
+
+	const Value* v = new Value(1, Type::TypeTag::INT);
+	tpsp->operand2(-1, v); // immediate value
+
+	tpsp->operandDest(get_tmp_stck_ptr_reg(), NULL);
+	tpsp->print();
+
+	// 4. store return address in some register
+	Instruction* returnIns;
+	returnIns= new MovIns(MovIns::MovInsType::LDI);
+	returnIns->operand1(get_tmp_stck_ptr_reg());
+
+	int newReg = intReg(); // get me next integer register
+
+	returnIns->operandDest(newReg, NULL);
+	returnIns->print();
+
+	//5. increment tmp stck by 1 so you can start storing parameters
+	Instruction* tpsp1 = new ArithIns(ArithIns::ArithInsType::ADD);
+
+	tpsp1->operand1(get_tmp_stck_ptr_reg(), NULL);
+	tpsp1->operand2(-1, v);
+
+	tpsp1->operandDest(get_tmp_stck_ptr_reg(), NULL);
+	tpsp1->print();
+
+	//6. start implementing codegen for parameters now
+	// opt for accessing the symbol table map 
+	SymTab* map = this->symTab();
+	for(SymTab::iterator i = map->begin(); i != map->end(); ++i){
+		VariableEntry* v = (VariableEntry*)(*i);
+		if(v->varKind==VariableEntry::VarKind::PARAM_VAR){
+			num_actual_param++;
+			v->codeGen();
+		}
+	// do the same for local variables.. if present
+	for(SymTab::iterator i = map->begin(); i != map->end(); ++i){
+		VariableEntry* v = (VariableEntry*)(*i);
+		if(v->varKind==VariableEntry::VarKind::LOCAL_VAR){
+			num_local_var++ ;
+			v->codeGen();
+
+		}
+
+	// codeGen for function body now
+
+	if(body()){
+		body()->codeGen();
+	}
+	// 7. update the base pointer through a sequence of statements
+	const Value* valBase = new Value(num_local_var, Type::TypeTag::INT);
+
+	Instruction* newBasePtIns = new ArithIns(ArithIns::ArithInsType::ADD);
+	newBasePtIns->operand1(get_tmp_stck_ptr_reg(), NULL); // current value of temp stack ptr
+	newBasePtIns->operand2(-1, valBase);
+
+	newBasePtIns->operandDest(get_tmp_stck_ptr_reg(), NULL);
+
+	newBasePtIns->print(); 
+
+	// 8. now you can perform load instruction to a temp reg
+	int anyReg = intReg();
+
+	Instruction* newLoadIns = new MovIns(MovIns::MovInsType::LDI);
+	newLoadIns->operand1(get_tmp_stck_ptr_reg(), NULL);
+	newLoadIns->operandDest(anyReg, NULL);  // loaded the value of temp stack pointer
+	newLoadIns->print();
+
+	// 9. finally update base pointer
+	Instruction* newBasePtrIns = new MovIns(MovIns::MovInsType::MOVI);
+	newBasePtrIns->operand1(anyReg, NULL);
+	newBasePtrIns->operandDest(get_tmp_stck_ptr_reg(), NULL);
+
+	newBasePtrIns->print();
+
+	// 9.a) before you update stack pointer -- please store its value in a register
+	// we may need to use this value during return address ?
+	regRetAddrLoad = intReg();
+    Instruction* retAdrLdIns = new MovIns(MovIns::MovInsType::LDI);
+    retAdrLdIns->operand1(get_stck_ptr_reg(), NULL);
+    retAdrLdIns->operandDest(regRetAddrLoad, NULL);
+
+	// 10. Update stack pointer also now :(
+	// Stack pointer updates by no. of local vars + 1 + no. of parameters
+	Instruction* newStackPtrIns = new ArithIns(ArithIns::ArithInsType::ADD);
+	newStackPtrIns->operand1(get_stck_ptr_reg(), NULL);
+
+	const Value* stackVal = new Value(num_actual_param + num_local_var + 1, Type::TypeTag::INT);
+
+	newStackPtrIns->operand2(-1, stackVal);
+	newStackPtrIns->operandDest(get_stck_ptr_reg(), NULL);
+
+	newStackPtrIns->print();
+
+	// Labels may not work !!?
+
+	newFuncRetLabel = Instruction::get_label();
+    setReturnLabel(newFuncRetLabel);
+
+	funcRetLabel = new LabelIns();
+    funcRetLabel->label(newFuncRetLabel);
+
+    funcRetLabel->print();
+
+    // 11. Now take the Jump !
+    Instruction* jumpFinalIns = new JumpIns(JumpIns::JumpInsType::JMPI);
+    jumpFinalIns->operandDest(regRetAddrLoad, NULL);
+
+    jumpFinalIns->print();
 
 
 }
